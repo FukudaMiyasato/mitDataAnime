dd0fc31f62ef75b5faafff37e5e8dff5b5d42036
fukudamiyasato <fukuda.miyasato@gmail.com>
2022-10-13 07:18:07 -0500

    pokeapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Fragment, useState, useEffect, useReducer } from 'react';
+import React from 'react';
+
+
+
 
 function App() {
+  const { useState, useEffect, useReducer } = React;
+  
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/ditto');
+  let [data,setData] = useState();
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      // Part 1, step 1 code goes here
+        //dispatch({type:'FETCH_INIT'});
+        try{
+          const result =await axios(url)
+          
+          setData(result.data);
+          console.log(data);
+          
+          if(!didCancel){
+          //  dispatch({type:'FETCH_SUCCESS',payload: result.data});
+          }
+        } catch (error){
+          if(!didCancel){
+          //  dispatch({type:'FETCH_FAILURE'});
+          }
+        }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url])
+
+const changePoke = ()=>{
+alert("cambiando el poke");
+setUrl('https://pokeapi.co/api/v2/pokemon/ditto');
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      
       </header>
+      {data}
+      <button className='mt-50' onClick={changePoke}>select pokemon</button>
     </div>
   );
 }
